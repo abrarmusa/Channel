c2e2060fc3a162ca26e065b0cbb09fa483dcba4e
Mimi Ko <o3d7@ugrad.cs.ubc.ca>
2016-03-09 08:52:06 -0800

    Separate chunk num from entire filename

--- src/splittest.go
+++ src/splittest.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"transfile"
+	"fmt"
 )
 
 func main() {
-	transfile.SplitFile("cat.jpg", 7)
+	transfile.SplitFile("dummy.txt", 6) // saves 6 chunks in ./
+	str, n := transfile.GetChunkNum("hey0re-rte.ertwer.jpg.34")
+	fmt.Println()
+	fmt.Println("Filename:", str)
+	fmt.Println("Chunk num:", n)
 }
--- src/transfile/split.go
+++ src/transfile/split.go
@@ -6,19 +6,7 @@ import (
 	"math"
 	"os"
 	"strconv"
-	"strings"
 )
-
-func getPrefix(s string) string {
-	if !strings.Contains(s, ".") {
-		return s
-	}
-	myarray := strings.Split(s, ".")
-	repeat := len(myarray) - 1
-	for i := 0; i < repeat; i++ {
-	}
-	return "whatever"
-}
 
 func SplitFile(filename string, num int) {
 	file, err := os.Open(filename)
@@ -26,14 +14,14 @@ func SplitFile(filename string, num int) {
 	defer file.Close()
 	fileinfo, _ := file.Stat()
 	var filesize uint64 = uint64(fileinfo.Size())
-	fmt.Println("Filesize:", filesize)
+	fmt.Println("Original file:", filename, ":", filesize)
 	totalpartnum := uint64(num)
 	part := uint64(math.Ceil(float64(filesize) / float64(totalpartnum)))
 	for i := uint64(1); i < totalpartnum+1; i++ {
 		partsize := int(math.Min(float64(part), float64(filesize - (i-1)*part)))
 		buf := make([]byte, partsize)
 		file.Read(buf)
-		partname := "doggy" + strconv.FormatUint(i, 10)
+		partname := filename + "." + strconv.FormatUint(i, 10)
 		_, err := os.Create(partname)
 		checkError(err)
 		ioutil.WriteFile(partname, buf, os.ModeAppend)
